36952e92efd9314718b937b340c61554cf685018
santanu <s.m.23102004@gmail.com>
2025-09-17 22:11:29 +0530

    add scroll and fix scaling for sharpenss

--- src/lib/components/PdfPage.svelte
+++ src/lib/components/PdfPage.svelte
@@ -4,67 +4,71 @@
 	let { pageProxy, page_no }: { pageProxy: PDFPageProxy; page_no: number } = $props();
 
 	let canvas_parent: HTMLDivElement | null = $state(null);
-
 	let canvas: HTMLCanvasElement;
-	let renderPage: (page: PDFPageProxy) => Promise<void>;
+	const dpr = window.devicePixelRatio || 1;
+	let renderPage: (page: PDFPageProxy) => void; // Function to render single page
 
 	$effect(() => {
-		// pdfjsLib.GlobalWorkerOptions.workerSrc = './pdf.worker.min.mjs';
-
 		let parentWidth = canvas_parent!.clientWidth;
 		let ctx = canvas.getContext('2d');
-		// let ctx: CanvasRenderingContext2D | null = canvas.getContext('2d');
 
-		renderPage = async (page: PDFPageProxy) => {
+		renderPage = (page: PDFPageProxy) => {
 			if (!ctx) {
 				console.error('Could not get 2D context from canvas');
 				return;
 			}
 			const unscaledViewport = page.getViewport({ scale: 1 });
-			const scale = parentWidth / unscaledViewport.width;
+			const scale = (parentWidth / unscaledViewport.width) * dpr;
+
 			var viewport = page.getViewport({ scale: scale });
+
+			// Canvas backing store size
 			canvas.height = viewport.height;
 			canvas.width = viewport.width;
 
-			// Render PDF page into canvas context
+			// CSS display size
+			canvas.style.height = `${viewport.height / dpr}px`;
+			canvas.style.width = `${viewport.width / dpr}px`;
 
 			var renderContext = {
 				canvasContext: ctx,
 				canvas: canvas,
 				viewport: viewport
 			};
-			var renderTask = page.render(renderContext);
-
-			// Wait for rendering to finish
-			// renderTask.promise.then(function () {
-			// 	pageRendering = false;
-			// 	if (pageNumPending !== null) {
-			// 		// New page rendering is pending
-			// 		renderPage(pageNumPending);
-			// 		pageNumPending = null;
-			// 	}
-			// });
+			page.render(renderContext);
 		};
 
 		renderPage(pageProxy);
 	});
-
-	// function queueRenderPage(num: number) {
-	// 	if (pageRendering) {
-	// 		pageNumPending = num;
-	// 	} else {
-	// 		renderPage(num);
-	// 	}
-	// }
 </script>
 
 <div class="parent-div" bind:this={canvas_parent}>
-	<canvas width="" bind:this={canvas} id="the-canvas"></canvas>
+	<span class="page-no">{page_no}</span>
+	<canvas bind:this={canvas} id="the-canvas"></canvas>
 </div>
 
 <style>
 	.parent-div {
 		width: 100%;
 		height: 100%;
+		position: relative;
+	}
+
+	.page-no {
+		width: fit-content;
+		height: fit-content;
+		padding: 2px 5px;
+		background: rgba(2, 24, 41, 0.379);
+		color: antiquewhite;
+		position: absolute;
+		left: 1px;
+		bottom: 1px;
+		border-top-right-radius: 6px;
+	}
+
+	#the-canvas {
+		display: block;
+		box-sizing: border-box;
+		border: 1px solid #0419162f;
 	}
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,25 @@
 <script lang="ts">
-    import PdfViewer from "$lib/PdfViewer.svelte";
-    export const ssr = false; 
+	import PdfViewer from '$lib/PdfViewer.svelte';
+	export const ssr = false;
 </script>
-<div class="pdf-viewer">
-    <PdfViewer url="./sample.pdf" />
+
+<div class="page">
+	<div class="pdf-viewer">
+		<PdfViewer url="./sample.pdf" />
+	</div>
 </div>
 
-
 <style>
-    .pdf-viewer{
-        width: 1200px;
-        height: auto;
-        aspect-ratio: 2/3;
-    }
+	.page {
+		width: 100svw;
+		height: 100svh;
+		display: flex;
+
+		align-items: center;
+		justify-content: center;
+	}
+	.pdf-viewer {
+		width: 800px;
+		height: 96%;
+	}
 </style>
